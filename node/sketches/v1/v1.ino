54e775b5d3e3808ac4ee00a2a0b1befba7f3a861
Trevor Stanhope <tpstanhope@gmail.com>
2015-02-06 02:41:45 -0500

    completed version 1 (untested)

--- node/sketches/v1/v1.ino
+++ node/sketches/v1/v1.ino
@@ -15,7 +15,9 @@
 #include "DallasTemperature.h"
 #include "OneWire.h"
 #include "stdio.h"
+#include "PID_v1.h"
 
+/* --- Constants --- */
 // I/O Pins
 const int N_PUMP_PIN = 2;
 const int CA_PUMP_PIN = 3;
@@ -37,19 +39,41 @@ const int BUFF_SIZE = 128;
 const int INTERVAL = 1000; // standardized delay between readings/adjustments
 const int SAMPLES = 4096;
 const int BAUD = 9600;
-const int PRECISION =2; // number of decimal places
-const int DIGITS = 4; // number of digits
+const int PRECISION = 2; // number of decimal places
+const int DIGITS = 6; // number of digits
+const int PH_DEFAULT = 512; // raw bit value at ideal pH
+const int EC_DEFAULT = 512; // raw bit value at ideal EC
+const int CA_DEFAULT = 512; // raw bit value at ideal Ca
+const int N_DEFAULT = 512; // raw bit value at ideal N
+const int K_DEFAULT = 512; // raw bit value at ideal K
 
-/* --- Declarations --- */
+// PID Values
+float N_P = 0.1, N_I = 0.0, N_D = 0.0;
+float Ca_P = 0.1, Ca_I = 0.0, Ca_D = 0.0;
+float K_P = 0.1, K_I = 0.0, K_D = 0.0;
+float EC_P = 0.1, EC_I = 0.0, EC_D = 0.0;
+float pH_P = 0.1, pH_I = 0.0, pH_D = 0.0;
+
+/* --- Variables --- */
 OneWire oneWire(TEMP_SENSOR_PIN);
 DallasTemperature temperature(&oneWire);
-char PH[CHARS];
+char pH[CHARS];
 char EC[CHARS];
 char N[CHARS];
-char CA[CHARS];
+char Ca[CHARS];
 char K[CHARS];
-char TEMP[CHARS];
-char OUTPUT_BUFFER[BUFF_SIZE];
+char temp[CHARS];
+char output_buffer[BUFF_SIZE];
+double N_set, N_in, N_out;
+PID N_pid(&N_in, &N_out, &N_set, N_P, N_I, N_D, DIRECT);
+double Ca_set, Ca_in, Ca_out;
+PID Ca_pid(&Ca_in, &Ca_out, &Ca_set, Ca_P, Ca_I, Ca_D, DIRECT);
+double K_set, K_in, K_out;
+PID K_pid(&K_in, &K_out, &K_set, K_P, K_I, K_D, DIRECT);
+double EC_set, EC_in, EC_out;
+PID EC_pid(&EC_in, &EC_out, &EC_set, EC_P, EC_I, EC_D, DIRECT);
+double pH_set, pH_in, pH_out;
+PID pH_pid(&pH_in, &pH_out, &pH_set, pH_P, pH_I, pH_D, DIRECT);
 
 /* --- Setup --- */
 void setup() {
@@ -69,28 +93,44 @@ void setup() {
   pinMode(EC_SENSOR_PIN, INPUT);
   pinMode(TEMP_SENSOR_PIN, INPUT);
 
-  Serial.begin(BAUD);
+  // Turn the PID on
+  N_set = N_DEFAULT;
+  Ca_set = CA_DEFAULT;
+  K_set = K_DEFAULT;
+  EC_set = EC_DEFAULT;
+  pH_set = PH_DEFAULT;
+  N_pid.SetMode(AUTOMATIC);
+  Ca_pid.SetMode(AUTOMATIC);
+  K_pid.SetMode(AUTOMATIC);
+  EC_pid.SetMode(AUTOMATIC);
+  pH_pid.SetMode(AUTOMATIC);
+  
+  // Start Temperature Sensors
   temperature.begin();
+  
+  // Start Serial
+  Serial.begin(BAUD);
 }
 
 /* --- Loop --- */
 void loop() {
-  send_data(); // reads sensors, then sends data
-  check_queue(); // gets command, then executes actions
+  read_sensors();
+  check_queue();
+  control_pumps();
   wait();
 }
 
-/* --- Send Data --- */
+/* --- Read Sensors --- */
 // Collects data and sends it to node.
-void send_data() {
-  dtostrf(test_temperature(), DIGITS, PRECISION, TEMP);
-  dtostrf(test_acidity(), DIGITS, PRECISION, PH);
+void read_sensors() {
+  dtostrf(test_temperature(), DIGITS, PRECISION, temp);
+  dtostrf(test_acidity(), DIGITS, PRECISION, pH);
   dtostrf(test_conductivity(), DIGITS, PRECISION, EC);
   dtostrf(test_nitrogen(), DIGITS, PRECISION, N);
-  dtostrf(test_calcium(), DIGITS,PRECISION, CA);
+  dtostrf(test_calcium(), DIGITS,PRECISION, Ca);
   dtostrf(test_potassium(), DIGITS, PRECISION, K);
-  sprintf(OUTPUT_BUFFER, "{'PH':%s,'EC':%s,'TEMP':%s,'N':%s,'CA':%s,'K':%s}",PH,EC,TEMP,N,CA,K); // concatenate message string
-  Serial.println(OUTPUT_BUFFER);
+  sprintf(output_buffer, "{'pH':%s,'EC':%s,'temp':%s,'N':%s,'Ca':%s,'K':%s}",pH,EC,temp,N,Ca,K); // concatenate message string
+  Serial.println(output_buffer);
   Serial.flush();
 }
 
@@ -99,14 +139,76 @@ void send_data() {
 void check_queue() {
   if (Serial.available()) {
     char command = Serial.read();
+    int set_point = Serial.parseInt();
     switch(command) {
-    case 1:
-      break;
-    default:
-      break;
+      case 'N':
+        N_set = set_point;
+        break;
+      case 'C':
+        Ca_set = set_point;
+        break;
+      case 'K':
+        K_set = set_point;
+        break;
+      case 'E':
+        EC_set = set_point;
+        break;
+      case 'P':
+        pH_set = set_point;
+        break;
+      case 'R':
+        N_set = N_DEFAULT;
+        Ca_set = CA_DEFAULT;
+        K_set = K_DEFAULT;
+        EC_set = EC_DEFAULT;
+        pH_set = PH_DEFAULT;
+        break;
+      default:
+        break;
     }
   Serial.flush();
   }
+}
+
+/* --- Control Pumps --- */
+// Calculate PID values and control pumps
+void control_pumps() {
+  N_pid.Compute();
+  Ca_pid.Compute();
+  K_pid.Compute();
+  pH_pid.Compute();
+  EC_pid.Compute();
+  if (N_out > N_set) {
+    digitalWrite(N_PUMP_PIN, HIGH);   // Add Nitrogen Solution  
+  }
+  else if (N_out < N_set) {
+    digitalWrite(N_PUMP_PIN, LOW);
+  }
+  if (Ca_out > Ca_set) {
+    digitalWrite(CA_PUMP_PIN, LOW);
+  }
+  else if (Ca_out < Ca_set) {
+    digitalWrite(CA_PUMP_PIN, HIGH);  // Add Calcium Solution
+  }
+  if (K_out > K_set) {
+    digitalWrite(K_PUMP_PIN, LOW);    
+  }
+  else if (K_out < K_set) {
+    digitalWrite(K_PUMP_PIN, HIGH); // Potassium Solution
+  }
+  if (EC_out > EC_set) {
+    digitalWrite(WATER_PUMP_PIN, HIGH);  // Add Water
+  }
+  else if (EC_out < EC_set) {
+    digitalWrite(WATER_PUMP_PIN, LOW);
+  }
+  if (pH_out > pH_set) {
+    digitalWrite(HCL_PUMP_PIN, HIGH);  // Add HCL Solution
+  }
+  else if (pH_out < pH_set) {
+    digitalWrite(HCL_PUMP_PIN, LOW);
+  }
+
 }
 
 /* --- Test Temperature --- */
@@ -119,56 +221,56 @@ float test_temperature() {
 
 /* --- Test pH --- */
 float test_acidity() {
-  float val = 0;
-  int reading = 0; 
+  long reading = 0; 
   for(int i = 0; i < SAMPLES; i++) {
     reading += analogRead(PH_SENSOR_PIN); 
   } 
-  reading /= SAMPLES;
+  float val = reading / SAMPLES;
+  pH_in = double(val); // #! Side effect
   return val;
 }
 
 /* --- Test EC --- */
 float test_conductivity() {
-  float val = 0;
-  int reading = 0; 
+  long reading = 0; 
   for(int i = 0; i < SAMPLES; i++) { // sample 100 times
     reading += analogRead(EC_SENSOR_PIN); 
   } 
-  reading /= SAMPLES;
+  float val = reading / SAMPLES;
+  EC_in = double(val); // #! Side effect
   return val;
 }
 
-/* --- Test Nitrogen --- */
+/* --- Test (N) Nitrogen --- */
 float test_nitrogen() {
-  float val = 0;
-  int reading = 0; 
+  long reading = 0; 
   for(int i = 0; i < SAMPLES; i++) { // sample 100 times
     reading += analogRead(N_SENSOR_PIN); 
   } 
-  reading /= SAMPLES;
+  float val = reading / SAMPLES;
+  N_in = double(val); // #! Side effect
   return val;
 }
 
-/* --- Test Calcium --- */
+/* --- Test (Ca) Calcium --- */
 float test_calcium() {
-  float val = 0;
-  int reading = 0; 
+  long reading = 0; 
   for(int i = 0; i < SAMPLES; i++) { // sample 100 times
     reading += analogRead(CA_SENSOR_PIN); 
   } 
-  reading /= SAMPLES;
+  float val = reading / SAMPLES;
+  Ca_in = double(val); // #! Side effect
   return val;
 }
 
-/* --- Test Potassium --- */
+/* --- Test (K) Potassium --- */
 float test_potassium() {
-  float val = 0;
-  int reading = 0; 
+  long reading = 0; 
   for(int i = 0; i < SAMPLES; i++) { // sample 100 times
     reading += analogRead(K_SENSOR_PIN); 
   } 
-  reading /= SAMPLES;
+  float val = reading / SAMPLES;
+  K_in = double(val); // #! Side effect
   return val;
 }
 
