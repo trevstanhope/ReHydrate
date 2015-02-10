beec01ebd970b14493735d698411e13bc94120f4
Trevor Stanhope <tpstanhope@gmail.com>
2015-02-10 17:28:26 -0500

    added arduino

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python-zmq mongodb python-dev build-essential python-cherrypy3 python-pip python-serial
+sudo apt-get install python-zmq mongodb python-dev build-essential python-cherrypy3 python-pip python-serial arduino
 sudo pip install pymongo
